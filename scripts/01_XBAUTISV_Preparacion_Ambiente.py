9a2d6d7197171da34d9cb546e3acf7b7b4625c01
xavierbautistav@gmail.com <xavierbautistav@gmail.com>
2026-01-18 02:53:51 +0000

    adiciono archivos nuevos

---
+++ scripts/01_XBAUTISV_Preparacion_Ambiente.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# DBTITLE 1,Cell 1
+# MAGIC %sql
+# MAGIC
+# MAGIC DROP CATALOG IF EXISTS xbautisv_prd CASCADE;
+# MAGIC
+# MAGIC CREATE CATALOG IF NOT EXISTS xbautisv_prd;
+# MAGIC
+# MAGIC CREATE SCHEMA IF NOT EXISTS xbautisv_prd.raw;
+# MAGIC CREATE SCHEMA IF NOT EXISTS xbautisv_prd.bronze;
+# MAGIC CREATE SCHEMA IF NOT EXISTS xbautisv_prd.silver;
+# MAGIC CREATE SCHEMA IF NOT EXISTS xbautisv_prd.gold;
+# MAGIC
+# MAGIC CREATE VOLUME IF NOT EXISTS xbautisv_prd.raw.datasets;
+# MAGIC
+# MAGIC CREATE EXTERNAL LOCATION IF NOT EXISTS `exlt-raw-prd`
+# MAGIC URL 'abfss://raw@adlxbautisvprd.dfs.core.windows.net/'
+# MAGIC WITH (STORAGE CREDENTIAL credentialxbautisvprd)
+# MAGIC COMMENT 'Ubicación externa para los archivos del raw del Data Lake';
+# MAGIC
+# MAGIC CREATE EXTERNAL LOCATION IF NOT EXISTS `exlt-bronze-prd`
+# MAGIC URL 'abfss://bronze@adlxbautisvprd.dfs.core.windows.net/'
+# MAGIC WITH (STORAGE CREDENTIAL credentialxbautisvprd)
+# MAGIC COMMENT 'Ubicación externa para los archivos del raw del Data Lake';
+# MAGIC
+# MAGIC CREATE EXTERNAL LOCATION IF NOT EXISTS `exlt-silver-prd`
+# MAGIC URL 'abfss://silver@adlxbautisvprd.dfs.core.windows.net/'
+# MAGIC WITH (STORAGE CREDENTIAL credentialxbautisvprd)
+# MAGIC COMMENT 'Ubicación externa para los archivos del raw del Data Lake';
+# MAGIC
+# MAGIC CREATE EXTERNAL LOCATION IF NOT EXISTS `exlt-golden-prd`
+# MAGIC URL 'abfss://golden@adlxbautisvprd.dfs.core.windows.net/'
+# MAGIC WITH (STORAGE CREDENTIAL credentialxbautisvprd)
+# MAGIC COMMENT 'Ubicación externa para los archivos del raw del Data Lake';
